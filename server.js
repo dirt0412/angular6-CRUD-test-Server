4ed3053b78e773674907c07f4948e7d94dd95469
dirt0412 <robertdrwecki@gmail.pl>
2019-03-26 19:45:20 +0100

    template string - query DB

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// 'use strict'
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 //Initiallising node modules
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -56,13 +59,13 @@ var executeQuery = function (res, query) {
 
 //GET API
 app.get("/api/product", function (req, res) {
-    var query = "SELECT id, name, description, price FROM [Table_products];";
+    var query = `SELECT id, name, description, price FROM [Table_products];`;
     executeQuery(res, query);
 });
 
 //GET API by id
 app.get("/api/product/:id", function (req, res) {
-    var query = "SELECT id, name, description, price FROM [Table_products] WHERE id = " + req.params.id + ";";
+    var query = `SELECT id, name, description, price FROM [Table_products] WHERE id = ${req.params.id};`;
     executeQuery(res, query);
 });
 
@@ -72,11 +75,9 @@ app.post("/api/product", function (req, res) {
         try {
             let pool = await sql.connect(dbConfig)
             let result = await pool.request()
-                .input('name', sql.NVarChar, req.body.product.name)
-                .input('description', sql.NVarChar, req.body.product.description)
-                .query("INSERT INTO [Table_products] (name, description, price) VALUES ( @name,@description,"+req.body.product.price+");")
+                .query(`INSERT INTO [Table_products] (name, description, price) VALUES ( '${req.body.product.name}','${req.body.product.description}',${req.body.product.price});`)
 
-                res.send(result);
+            res.send(result);
 
             // // Stored procedure            
             // let result2 = await pool.request()
@@ -102,11 +103,8 @@ app.put("/api/product/:id", function (req, res) {
         try {
             let pool = await sql.connect(dbConfig)
             let result = await pool.request()
-                .input('name', sql.NVarChar, req.body.product.name)
-                .input('description', sql.NVarChar, req.body.product.description)
-                .query("UPDATE [Table_products] SET name=@name , description=@description , price=" + req.body.product.price + "  WHERE id= " + req.params.id + ";")
-
-                res.send(result);
+                .query(`UPDATE [Table_products] SET name='${req.body.product.name}', description='${req.body.product.description}', price=${req.body.product.price}  WHERE id= ${req.params.id};`)
+            res.send(result);
         } catch (err) {
             console.log("Error : " + err);
             res.send(err);
@@ -120,6 +118,78 @@ app.put("/api/product/:id", function (req, res) {
 
 // DELETE API
 app.delete("/api/product/:id", function (req, res) {
-    var query = "DELETE FROM [Table_products] WHERE id=" + req.params.id + ";";
+    var query = `DELETE FROM [Table_products] WHERE id=${req.params.id};`;
     executeQuery(res, query);
+});
+
+//GET API by mail
+app.get("/api/users/:mail", function (req, res) {
+    var query = `SELECT mail, password FROM [Table_users] WHERE mail = '${req.params.mail}';`;
+    executeQuery(res, query);
+});
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'mail',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function (req, mail, password, done) {
+        mail = mail.toLowerCase()
+        let user = []
+        var dbConn = new sql.Connection(dbConfig);
+        dbConn.connect().then(function () {
+            var request = new sql.Request(dbConn);
+            request.query(`SELECT mail FROM [Table_users] WHERE mail = '${mail}';`).then(function (recordSet) {
+                if (recordSet.length > 0) {
+                    user.mail = recordSet[0].mail;
+                }
+                dbConn.close();
+                return done(null, user)
+            }).catch(function (err) {
+                console.log(err);
+                dbConn.close();
+            });
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+        return done(null, user)
+    }
+))
+
+// POST API login
+app.post("/api/login", function (req, res, next) {
+    let response = {
+        status: false
+    }
+    if (!req.body.mail) {
+        response.error = 'Email can\'t be blank'
+        return res.json(response)
+    }
+    if (!req.body.password) {
+        response.error = 'Password can\'t be blank'
+        return res.json(response)
+    }
+    passport.authenticate('local', {}, async function (err, user, info) {
+        if (err) {
+            return next(err)
+        }
+        let user1 = await outputUser(user);
+        if (user1.mail !== undefined) {
+            // user1.lastloginAt = Date.now()
+            // req.session.user = user1
+            response.status = true
+            response.user = user1
+            return res.json(response)
+        }
+    })(req, res, next)
+
+    async function outputUser(user) {
+        return {
+            mail: user.mail,
+            password: user.password
+        }
+
+    }
 });
